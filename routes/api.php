b03acc9c8a03fa98d3f9dbfbb5d1d58db0c616d8
russik2013 <z.kon2009@yandex.ru>
2018-08-13 01:25:21 +0300

    Install project and add api system

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::post('user/register', 'APIRegisterController@register');
+Route::post('user/login', 'APILoginController@login');
+
+Route::middleware('jwt.auth')->get('users', function(Request $request) {
+    return auth()->user();
+});
+
+Route::group(["middleware" => "jwt.auth"], function(){
+    Route::post("auth/logout", "APILoginController@logout");
+});
